6ac07efa570d6f8cb29e0d495884bb2bb33e1845
ywk <987378653@qq.com>
2019-07-09 22:43:27 +0800

    1.增加通知优先级，长文本，图片显示，灯光，震动，声音等功能

--- app/src/main/java/com/example/mynotification/MainActivity.java
+++ app/src/main/java/com/example/mynotification/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = new Intent(this , NotificationActivity.class);
         PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
         NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(1);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             String channelId = "default";
             String channelName = "默认通知";
@@ -40,7 +42,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                     .setSmallIcon(R.mipmap.ic_launcher)
                                     .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
                                     .setContentIntent(pi)
+                                    .setVibrate(new long[]{0, 1000, 1000, 1000})
+                                    .setLights(Color.GREEN, 1000, 1000)
+                                    .setDefaults(NotificationCompat.DEFAULT_ALL)
+                                    .setStyle(new NotificationCompat.BigTextStyle().bigText("这是一个美好的夜晚，我一个人在电脑面前写代码"))
+//                                    .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background)))
+                                    .setPriority(NotificationCompat.PRIORITY_MIN)
                                     .build();
         manager.notify(1,notification);
+
     }
 }
